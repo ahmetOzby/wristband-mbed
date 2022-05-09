fc19cc5fb20be4d339e13bd6c1ec0839bafb04df
ahmetOzby <ahmetozbay99@gmail.com>
2022-05-09 18:31:27 +0300

    v0.5
    MQTT ve WiFi bağlantısı dinamik olarak kurulacak şekilde ayarlandır. Bağlantılar belirli aralıklarla kontrol edilerek bağlantı kopuşunda tekrar bağlantı kurulması sağlanıyor. 200 ms'lik taskta örnek mqtt mesajı iletildi.

--- smart_wristband_mbed/smart_wristband_mbed.ino
+++ smart_wristband_mbed/smart_wristband_mbed.ino
@@ -3,7 +3,9 @@
 #include "src/tft.h"
 #include "src/mqtt.h"
 #define _1_sec_in_ms  1000
+#define _200_ms       200
 #define _1_hour_in_ms 360000
+#define _10_sec_in_ms 10000
 
 extern uint8_t ss;
 extern uint8_t mm;
@@ -12,34 +14,59 @@ extern bool initial;
 
 typedef struct
 {
+  uint32_t _200_ms_timer;
   uint32_t _1_sec_timer;
+  uint64_t _10_sec_timer;
   uint64_t _1_hour_timer;
 }task_timer_t;
 
 task_timer_t task_timer;
-WiFiClient client;
+WiFiClient espClient;
+PubSubClient client(espClient);
 WiFiUDP ntpUDP;                                                       //UDP nesnesi oluşturulur.
 NTPClient timeClient(ntpUDP, "tr.pool.ntp.org");                     //NTP'yi kullanacak UDP nesnesi seçilir.
 void setup(void) 
 {
   Serial.begin(9600);
   WiFi.begin("Ahmet", "11111111");
-
+  client.setServer("broker.mqttdashboard.com", 1883);
   clock_gui_init();
   // time init
 
   task_timer._1_sec_timer = millis() + _1_sec_in_ms;
-  task_timer._1_hour_timer = millis() + _1_sec_in_ms * 20;
+  task_timer._1_hour_timer = millis() + _10_sec_in_ms;
+  task_timer._1_hour_timer = millis() + _10_sec_in_ms;
 }
 
 void loop() 
 {
+  if (task_timer._200_ms_timer < millis()) 
+  {
+    Serial.println("200ms task initated");
+    if(WiFi.status() == WL_CONNECTED && client.connected() == true)
+    {
+      client.publish("s_wristband", (const uint8_t *)"1?aZ", 4); 
+    }
+    task_timer._200_ms_timer += _200_ms;
+  }
+
+  
   if (task_timer._1_sec_timer < millis()) 
   {
     Serial.println("1 sec task initated");
     Serial.println(WiFi.status());
-    task_timer._1_sec_timer += _1_sec_in_ms;
     start_clock();
+    task_timer._1_sec_timer += _1_sec_in_ms;
+  }
+
+  if (task_timer._10_sec_timer < millis()) 
+  {
+    if(WiFi.status() == WL_CONNECTED && client.connected() == false)
+    {
+      client.connect("ESP8266Ahmet");
+      Serial.print("mqttye bağlan komutu çalıştırıldı.");
+    }
+    task_timer._10_sec_timer += _10_sec_in_ms;
   }
 
   if (task_timer._1_hour_timer < millis()) 
